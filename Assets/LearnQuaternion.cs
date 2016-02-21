096e4518567da92e80111afe09a8bfa225a8f532
dantran304 <dantrandinhh@gmail.com>
2016-02-21 22:40:49 +0700

    upload project

---
+++ Assets/LearnQuaternion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LearnQuaternion : MonoBehaviour {
+    public Transform target;
+
+
+    public Transform from;
+    public Transform to;
+    public float speed = 0.1f;
+    public Quaternion qrotation = Quaternion.Euler(0, 30, 0);
+	// Update is called once per frame
+	void Update () {
+        //Vector3 relativePos = target.position - transform.position;
+        //Quaternion rotation = Quaternion.LookRotation(relativePos);
+        //transform.rotation = rotation;
+
+        //tra ve gia tri rotation chenh lech giua 2 vat the
+        //float angle = Quaternion.Angle(transform.rotation, target.rotation);
+        //Debug.Log("Angle = " + angle);
+
+        // xoay vat the theo 1 goc dat truoc
+        //transform.rotation = qrotation;
+
+        //quay vat the tu goc' from --> goc' to  voi' toc' do. dat truoc
+        //transform.rotation = Quaternion.Slerp(from.rotation, to.rotation, speed * Time.time);
+
+        //deo hieu
+        //transform.rotation = Quaternion.FromToRotation(transform.up, transform.right);
+
+        //dieu chinh rotation ve default 0 0 0
+        //transform.rotation = Quaternion.identity;
+        //Debug.Log("Quaternion identity = " + Quaternion.identity);
+
+        
+	}
+}
---
+++ Assets/MoveBall.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBall : MonoBehaviour {
+
+    private Rigidbody rigid;
+    public float moveSpeed;
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+	}
+
+
+	
+	// Update is called once per frame
+	void Update()
+    {
+        AnimationControl();
+    }    
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigid.AddForce(movement*moveSpeed);
+        Debug.Log("Move!");
+    }
+
+    void AnimationControl()
+    {
+        Debug.Log("Anim!");
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 0f, 0f));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, -90, 0f));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 90, 0f));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 180, 0f));
+        }
+
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isMove", true);
+        }
+        anim.SetBool("isMove", false);
+    }
+}
